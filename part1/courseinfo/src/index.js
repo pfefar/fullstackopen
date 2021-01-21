19c5def41bd8a2a7361385207b3c10d5334ac5fd
Prince Fefar <pfefar1@gmail.com>
2021-01-21 10:34:06 -0500

    1.5 added course and its parts to single javascript object

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => (
-  <h1>{props.course}</h1>
+  <h1>{props.course.name}</h1>
 )
 
 const Part = (props) => {
@@ -35,27 +35,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    part: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part} />
-      <Total part={part} />
+      <Content part={course.part} />
+      <Total part={course.part} />
     </div>
   )
 }
