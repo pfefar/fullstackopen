d13f60d8caa857d569549ce31b6167913592150f
Prince Fefar <pfefar1@gmail.com>
2021-01-21 09:21:09 -0500

    refactored variables of App into a object

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,6 +6,7 @@ const Header = (props) => (
 )
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
       <p>
@@ -33,23 +34,29 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content name1={part1}
-        excount1={exercises1}
-        name2={part2}
-        excount2={exercises2}
-        name3={part3}
-        excount3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content name1={part1.name}
+        excount1={part1.exercises}
+        name2={part2.name}
+        excount2={part2.exercises}
+        name3={part3.name}
+        excount3={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
 
     </div>
   )
