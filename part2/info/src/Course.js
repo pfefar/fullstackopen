12930ca5ccf2e370665e3499485deabb2e98a9b6
Prince Fefar <pfefar1@gmail.com>
2021-02-02 14:54:41 -0500

    2.4 and 2.5 step8 and step9

--- part2/info/src/Course.js
+++ part2/info/src/Course.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import Header from './Header';
-import Content from './Content';
-import Total from './Total';
+import React from 'react'
 
-const Course = ({ course }) => {
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((s, v) => s + v.exercises, 0)
+
+
+  return (
+    <p><strong>total of exercises {sum}</strong></p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
     </div>
   )
 }
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 export default Course
